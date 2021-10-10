239f36a0c63ba2dc94933e158b498ee9e9abb512
Sarthak Kamble <sarthakkamble412@gmail.com>
2021-10-10 22:09:43 +0530

    Responsive Navbar Commit

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,20 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('nav_links')[0]
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const navbarLinks = document.getElementsByClassName('nav_links')[0]
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+// toggleButton.addEventListener('click', () => {
+//     navbarLinks.classList.toggle('active')
+// })
+
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = ()=>{
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+}
+
+cancelBtn.onclick = ()=>{
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+}
