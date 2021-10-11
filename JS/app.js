f227baae74e7ebe1b15d359ae50d62218e4d63da
Sarthak Kamble <sarthakkamble412@gmail.com>
2021-10-11 20:32:01 +0530

    Responsive Design

--- JS/app.js
+++ JS/app.js
@@ -8,13 +8,18 @@
 const menu = document.querySelector(".menu-list");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+const body = document.querySelector("body");
 
 menuBtn.onclick = ()=>{
     menu.classList.add("active");
     menuBtn.classList.add("hide");
+    body.classList.add("disabledScroll");
 }
 
 cancelBtn.onclick = ()=>{
     menu.classList.remove("active");
     menuBtn.classList.remove("hide");
+    body.classList.remove("disabledScroll");
 }
+   
+
